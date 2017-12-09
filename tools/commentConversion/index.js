77f725333e6f23d723181ecb2fd58011a6cba0f7
mwinata94 <mwinata@mail.sfsu.edu>
2017-12-08 16:12:28 -0800

    Update Page

--- tools/commentConversion/index.js
+++ tools/commentConversion/index.js
@@ -1,20 +1,6 @@
 $(document).ready(function() {
 
 	// Toggle dropdown
-	$("#navbar .head_wlist").click(function(event) {
-		if (!$(event.target).parent().hasClass("show")) {
-			$(this).find(".list").toggleClass("show");
-		} else {
-			var t = $(this).find("span")[0].innerHTML + "/" + $(event.target).text();
-			if (t == "Tools/comment") {
-				window.location.href = "././index.html";
-			}
-			if (t == "Tools/color") {
-				window.location.href = "../colorTransform/index.html";
-			}
-		}
-	});
-
 	$("#navbar .head_wolist").click(function() {
 		var t = $(this).find("span").text();
 		if (t == "MW94") {
