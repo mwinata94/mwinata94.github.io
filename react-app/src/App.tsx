8f19aef6f0aace07899e76616d74d90a3502b6d3
Michael Winata <mwinata@mail.sfsu.edu>
2019-05-24 23:22:21 -0700

    add typescript to react-app

---
+++ react-app/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+class App extends React.Component {
+  render = () => <div className='App hero is-fullheight is-dark' />;
+}
+
+export default App;
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
