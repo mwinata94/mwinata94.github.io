666d5657aa3d0eab437431ecb2898e742c273fd1
Michael Winata <mwinata@mail.sfsu.edu>
2019-06-09 10:32:33 -0700

    update about content

--- react-app/src/Menu/Context/aboutContext.tsx
+++ react-app/src/Menu/Context/aboutContext.tsx
@@ -5,8 +5,7 @@ export const aboutContext: import('./IContext').IContext = (): JSX.Element => (
     <br />
     <span className='text'>
       Hi, welcome to my website. My name's Michael Winata, and I'm a Computer
-      Science student at San Francisco State University. Feel free to browse my
-      craft.
+      Science graduate. Feel free to browse my craft.
     </span>
   </div>
 );
