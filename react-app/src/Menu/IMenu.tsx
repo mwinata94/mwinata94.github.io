cb1a20cd8e01921daf0ffb934cf9d3f9c3aba0c0
Michael Winata <mwinata@mail.sfsu.edu>
2019-06-09 12:01:09 -0700

    refactor React.CSSProperties to ICSSProperties

--- react-app/src/Menu/IMenu.tsx
+++ react-app/src/Menu/IMenu.tsx
@@ -1,10 +1,12 @@
+import { ICSSProperties } from './ICSSProperties';
+
 export interface IMenu {
   getIconFaName(): string;
   getContext(): import('./Context').IContext;
-  getStyleActivate(): import('react').CSSProperties;
-  getStyleDeactivate(): import('react').CSSProperties;
-  getStyleShow(): import('react').CSSProperties;
-  getStyleHide(): import('react').CSSProperties;
+  getStyleActivate(): ICSSProperties;
+  getStyleDeactivate(): ICSSProperties;
+  getStyleShow(): ICSSProperties;
+  getStyleHide(): ICSSProperties;
   onClick(): void;
   activate(): void;
   deactivate(): void;
@@ -18,5 +20,5 @@ export interface IMenuProps {
 
 export interface IMenuState {
   activate: boolean;
-  style: import('react').CSSProperties;
+  style: ICSSProperties;
 }
--- react-app/src/Menu/MainMenu.tsx
+++ react-app/src/Menu/MainMenu.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
 import { IMenu } from './IMenu';
 import { Menu } from './Menu';
+import { ICSSProperties } from './ICSSProperties';
 
 export class MainMenu extends Menu implements IMenu {
   getIconFaName = (): string => 'fa fa-power-off';
   getContext(): import('./Context').IContext {
     throw new Error('Method not implemented.');
   }
-  getStyleActivate(): React.CSSProperties {
+  getStyleActivate(): ICSSProperties {
     throw new Error('Method not implemented.');
   }
-  getStyleDeactivate(): React.CSSProperties {
+  getStyleDeactivate(): ICSSProperties {
     throw new Error('Method not implemented.');
   }
-  getStyleShow(): React.CSSProperties {
-    throw new Error('Method not implemented.');
+  getStyleShow(): ICSSProperties {
+    return {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      '&:hover': {
+        transform: 'scale(1.2)'
+      }
+    };
   }
-  getStyleHide(): React.CSSProperties {
+  getStyleHide(): ICSSProperties {
     throw new Error('Method not implemented.');
   }
   onClick(): void {
