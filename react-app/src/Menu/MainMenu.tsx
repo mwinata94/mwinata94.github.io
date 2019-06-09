e81d049810162a853411fe20019910e847a67e79
Michael Winata <mwinata@mail.sfsu.edu>
2019-06-09 12:54:13 -0700

    revert back some arrow functions to default functions to fix constructor error

--- react-app/src/Menu/MainMenu.tsx
+++ react-app/src/Menu/MainMenu.tsx
@@ -5,20 +5,20 @@ import { CSSProperties } from 'react';
 
 export class MainMenu extends Menu implements IMenu {
   getIconFaName = (): string => 'fa fa-power-off';
-  getContext = (): IContext => {
+  getContext(): IContext {
     throw new Error('Method not implemented.');
-  };
-  getStyleActivate = (): CSSProperties => {
+  }
+  getStyleActivate(): CSSProperties {
     throw new Error('Method not implemented.');
-  };
-  getStyleDeactivate = (): CSSProperties => {
+  }
+  getStyleDeactivate(): CSSProperties {
     throw new Error('Method not implemented.');
-  };
-  getStyleShow = (): CSSProperties => {
+  }
+  getStyleShow(): CSSProperties {
     return {
       borderRadius: '50%',
-      width: '2em',
-      height: '2em',
+      width: '1.75em',
+      height: '1.75em',
       fontSize: '3em',
       cursor: 'pointer',
       display: 'flex',
@@ -28,10 +28,10 @@ export class MainMenu extends Menu implements IMenu {
       color: 'rgba(255, 255, 255, 0.6)',
       transition: 'background 0.3s, color 0.3s, transform 0.3s'
     };
-  };
-  getStyleHide = (): CSSProperties => {
+  }
+  getStyleHide(): CSSProperties {
     throw new Error('Method not implemented.');
-  };
+  }
   onClick = (): void => {
     if (this.state.activate) {
       this.deactivate();
