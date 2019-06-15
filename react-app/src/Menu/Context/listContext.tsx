4486f7443d605beb986191ed359892a52ec7a969
Michael Winata <mwinata@mail.sfsu.edu>
2019-06-15 15:27:06 -0700

    update list context

--- react-app/src/Menu/Context/listContext.tsx
+++ react-app/src/Menu/Context/listContext.tsx
@@ -8,17 +8,72 @@ export const listContext: import('./IContext').IContext = (
       <p className='card-header-title'>Latest Crafts:</p>
     </header>
     <div className='card-content'>
-      <span className='text'>
-        <a href='https://mwinata94.github.io/tools/commentConversion/index.html'>
-          Comment Conversion
-        </a>
-      </span>
-      <br />
-      <span className='text'>
-        <a href='https://mwinata94.github.io/tools/colorTransform/index.html'>
-          Color Transform
-        </a>
-      </span>
+      <div className='box'>
+        <div className='columns'>
+          <div className='column'>
+            <p className='is-size-4'>Comment Conversion</p>
+            <p>Convert text into a comment block</p>
+          </div>
+          <div className='column is-narrow flex-center-content'>
+            <a
+              className='button is-large is-primary'
+              href='https://mwinata94.github.io/tools/commentConversion/'
+            >
+              Open
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='columns'>
+          <div className='column'>
+            <p className='is-size-4'>Color Transform</p>
+            <p>
+              Cycle element's color between color a and color b at x interval
+            </p>
+          </div>
+          <div className='column is-narrow flex-center-content'>
+            <a
+              className='button is-large is-primary'
+              href='https://mwinata94.github.io/tools/colorTransform/'
+            >
+              Open
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='columns'>
+          <div className='column'>
+            <p className='is-size-4'>LitLister</p>
+            <p>e-commerce web application</p>
+          </div>
+          <div className='column is-narrow flex-center-content'>
+            <a
+              className='button is-large is-primary'
+              href='https://litlister.com'
+            >
+              Open
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='box'>
+        <div className='columns'>
+          <div className='column'>
+            <p className='is-size-4'>Dealiooo</p>
+            <p>monopoly deal simulator</p>
+          </div>
+          <div className='column is-narrow flex-center-content'>
+            <a
+              className='button is-large is-primary'
+              href='https://dealiooo.herokuapp.com/'
+            >
+              Open
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
