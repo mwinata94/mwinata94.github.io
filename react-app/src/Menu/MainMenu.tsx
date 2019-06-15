d18b7fc4aa2ac1c11d9a1e67730627fa22b27535
Michael Winata <mwinata@mail.sfsu.edu>
2019-06-15 11:28:17 -0700

    remove excessive interface implementation

--- react-app/src/Menu/MainMenu.tsx
+++ react-app/src/Menu/MainMenu.tsx
@@ -1,13 +1,11 @@
 import { IContext } from './Context';
-import { IMenu } from './IMenu';
 import { Menu } from './Menu';
 import React from 'react';
 
-export class MainMenu extends Menu implements IMenu {
+export class MainMenu extends Menu {
   getIconFaName = (): string => 'fa fa-power-off';
   getContext = (): IContext => (): JSX.Element => <></>;
   getMenuName = (): string => 'main';
-
   onClick = (): void => {
     this.toggleClassActivated();
     this.state.activated
